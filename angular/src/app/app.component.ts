1a04d243665626bde56b26eeccb3dc0dc16d975e
Koray <koray.akyurek6@gmail.com>
2021-06-29 11:10:45 +0200

    EXERCICE 2

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent{
+  title = 'angular';
+  public statistique1: Statistique;
+  public statistique2: Statistique;
+
+  constructor(){
+    this.statistique1 = new Statistique('“fa1f5f40','Popa','60293','DEFET')
+    this.statistique2 = new Statistique('“fa68546','Pop1','60','DE')
+  }
+}
+
